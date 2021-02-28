f7c45f5773517932e3c06652f6af6cb14f8ae9ee
anhdt <24364347+taddy569@users.noreply.github.com>
2021-02-28 21:03:58 +0700

    refactor/react-hook

--- src/components/layout/Alert.jsx
+++ src/components/layout/Alert.jsx
@@ -1,6 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
 
-const Alert = ({ alert }) => {
+import AlertContext from "../../context/alert/alertContext";
+
+const Alert = () => {
+  const { alert } = useContext(AlertContext);
+
   return (
     alert !== null && (
       <div className={`alert alert-${alert.type}`}>
